f809572acaebd3a4d9bcadbe3e9a410a32e2e4ca
Cxp <Cxp@DESKTOP-13C99QB>
2018-11-19 16:07:50 +0800

    404 update

--- graduation/src/main/java/cxp/graduate/net/SocketServiceLoader.java
+++ graduation/src/main/java/cxp/graduate/net/SocketServiceLoader.java
@@ -1,23 +1,25 @@
 package cxp.graduate.net;
 
-import static org.hamcrest.CoreMatchers.nullValue;
-
-import java.net.ServerSocket;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import cxp.graduate.net.SocketThread;
+
 public class SocketServiceLoader implements ServletContextListener {
 
 	private SocketThread socketThread;
-	/**
-	 * 初始化服务器线程
-	 */
+
 	@Override
-	public void contextInitialized(ServletContextEvent sce) {
+	public void contextDestroyed(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
-		ServletContext servletContext = sce.getServletContext();
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		ServletContext servletContext = arg0.getServletContext();
 		String str = (String) servletContext.getAttribute("SocketThreadRun");//获取字段（是否存在）
 		if(socketThread == null && str ==null) {
 			servletContext.setAttribute("SocketThreadRun", "true");
@@ -25,11 +27,5 @@ public class SocketServiceLoader implements ServletContextListener {
 			Thread thread = new Thread(socketThread);
 			thread.start();
 		}
-	}
-	
-	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
-		ServletContextListener.super.contextDestroyed(sce);
 	}
 }
